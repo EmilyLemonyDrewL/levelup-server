fcaa3cc4020785f79f7ef937818f312407b8355a
Emily Drew L <DontDenyHighFives@gmail.com>
2023-11-20 19:41:45 -0600

    Chapter 10 complete

--- levelup/urls.py
+++ levelup/urls.py
@@ -1,3 +1,9 @@
+from django.conf.urls import include
+from django.contrib import admin
+from django.urls import path
+from rest_framework import routers
+from levelupapi.views import GameTypeView, GameView, EventView, register_user, check_user
+
 """levelup URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -13,9 +19,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+
+
+router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'gametypes', GameTypeView, 'gametype')
+router.register(r'games', GameView, 'game')
+router.register(r'events', EventView, 'event')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+    path('register', register_user),
+    path('checkuser', check_user),
 ]
